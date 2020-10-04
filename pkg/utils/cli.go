565c8359f04db38c442ce88f0f275e43116f5458
jinified <jinified@gmail.com>
2020-10-04 15:30:10 +0800

    Create utils package

---
+++ pkg/utils/cli.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+     "fmt"
+     "github.com/spf13/cobra"
+)
+
+func NewCLI(app string, desc string) {
+    return &cobra.Command {
+        Use: app,
+        Short: desc,
+        Run: func (cmd *cobra.Command, args []string) {
+            fmt.Printf("Running %s\n", app)
+        },
+    }
+}

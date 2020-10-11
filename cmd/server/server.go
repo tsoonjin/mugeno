a98b9c0a0b04d0fe9b42c625620961d7a7af3fbc
jinified <jinified@gmail.com>
2020-10-11 14:41:29 +0800

    Server initiated

---
+++ cmd/server/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    "net/http"
+    "log"
+    "os"
+    "github.com/joho/godotenv"
+    "github.com/go-chi/chi"
+    "github.com/go-chi/chi/middleware"
+)
+
+type Config struct {
+    MiddlewareTimeout int
+    ServerPort string
+}
+
+var config Config;
+
+func init() {
+    log.Println("Loading environment variables ...")
+     if err := godotenv.Load(); err != nil {
+        log.Println("No .env file found")
+    }
+    serverPort, _ := os.LookupEnv("SERVER_PORT")
+    config = Config{
+        ServerPort: serverPort,
+    }
+}
+
+func main() {
+    r := chi.NewRouter()
+    // A good base middleware stack
+    r.Use(middleware.RequestID)
+    r.Use(middleware.RealIP)
+    r.Use(middleware.Logger)
+    r.Use(middleware.Recoverer)
+    r.Use(middleware.Timeout(60 * time.Second))
+
+    r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Println("Health check")
+        w.Write([]byte("OK"))
+    })
+
+    // Run Server
+    http.ListenAndServe(fmt.Sprintf(":%v", config.ServerPort), r)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/tsoonjin/mugeno
 go 1.14
 
 require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/joho/godotenv v1.3.0
 	github.com/spf13/cobra v1.0.0
 	github.com/thatisuday/commando v1.0.4
 )
